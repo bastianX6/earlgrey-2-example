d02f40d91ab1470e61c9b8bd5967756c2b1a1054
Bastian Veliz <bastianx6@gmail.com>
2020-05-21 17:17:26 -0400

    Add project and network client

---
+++ ItunesSimpleSearch/ItunesSimpleSearch/Core/DataSource/ItunesDataSoruce.swift
@@ -0,0 +1,9 @@
+//
+//  ItunesDataSoruce.swift
+//  ItunesSimpleSearch
+//
+//  Created by Bastián Véliz Vega on 21-05-20.
+//  Copyright © 2020 Bastián Véliz Vega. All rights reserved.
+//
+
+import Foundation
